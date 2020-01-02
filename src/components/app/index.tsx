4523cf276a4481e5ae23827f551a727e369c5ea7
Asa Ayers <Asa@AsaAyers.com>
2020-01-02 08:15:48 -0800

    Edit needs to be the last route

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {
   Switch,
   Route,
-  Link,
-  useLocation,
 } from "react-router-dom";
 import { Routes } from '../../constants'
 import Edit from '../edit'
@@ -13,14 +11,14 @@ import AssembleSecret from '../assemble'
 export default function App() {
   return (
     <Switch>
-      <Route path={Routes.Edit}>
-        <Edit />
-      </Route>
       <Route path={Routes.Print}>
         <Print />
       </Route>
       <Route path={Routes.Assemble}>
         <AssembleSecret />
+      </Route>
+      <Route path={Routes.Edit}>
+        <Edit />
       </Route>
     </Switch>
   );
