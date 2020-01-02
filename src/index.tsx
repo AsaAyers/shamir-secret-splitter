ac1f66c473393cee169214a434dc5af0b27a487d
Asa Ayers <Asa@AsaAyers.com>
2020-01-01 17:13:17 -0800

    Add basename to fix routing on gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import './serviceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const basename = '/shamir-secret-splitter'
+
+ReactDOM.render(<Router basename={basename}><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
