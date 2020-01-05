2a6c2800a0362ff5a9c9abd32d2eab8789c7efaf
Asa Ayers <Asa@AsaAyers.com>
2020-01-05 10:01:10 -0800

    Updates

---
+++ precache-manifest.5a864afe0d2859c6384ccf879aa37dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016604a860bc10725a7e525fd073d40f",
+    "url": "/shamir-secret-splitter/index.html"
+  },
+  {
+    "revision": "eabac758b3ba5907dc62",
+    "url": "/shamir-secret-splitter/static/css/main.023d1f0d.chunk.css"
+  },
+  {
+    "revision": "a6c859aed7a07d55a00c",
+    "url": "/shamir-secret-splitter/static/js/2.251e77ca.chunk.js"
+  },
+  {
+    "revision": "cc08cf93cc1fae2b308c598502122ede",
+    "url": "/shamir-secret-splitter/static/js/2.251e77ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eabac758b3ba5907dc62",
+    "url": "/shamir-secret-splitter/static/js/main.1cecae4c.chunk.js"
+  },
+  {
+    "revision": "03e0d94cf782add5fd3b",
+    "url": "/shamir-secret-splitter/static/js/runtime-main.1f08e6b3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shamir-secret-splitter/precache-manifest.5e78c9eaadd75faab5c15c4874019f58.js"
+  "/shamir-secret-splitter/precache-manifest.5a864afe0d2859c6384ccf879aa37dc5.js"
 );
 
 self.addEventListener('message', (event) => {
